3167ad4f511c062c293d39c310205f53f3657659
Raphael <phael92br@gmail.com>
2022-02-09 13:52:41 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+// Importar o express
+const express = require("express");
+
+// Criar um servidor: app
+const app = express();
+
+// Criar uma rota "/" que vai enviar um arquivo views/index.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Rodas o servidor na porta 3000
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
