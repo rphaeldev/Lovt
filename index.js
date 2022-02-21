187439f3a7accd5fb8daf006f6f822712214e077
Raphael <phael92br@gmail.com>
2022-02-21 10:32:40 -0300

    introduzinho css ao html

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const express = require("express");
 // Criar um servidor: app
 const app = express();
 
+// Definindo a pasta de arquivos estáticos
+app.use(express.static(__dirname+"/public"))
+
 // Criar uma rota "/" que vai enviar um arquivo views/index.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+
 
 // Rodas o servidor na porta 3000
 app.listen(3000, () => {
